5da7c34aaf2188e44e9b87a4459b36096d6ab0be
Denis Kaynar <kaynar.denis@gmail.com>
2023-07-09 21:16:12 +0300

    fix

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -12,10 +12,8 @@ builder.Configuration.AddJsonFile("appsettings.json");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
-  app.UseSwagger();
-  app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
